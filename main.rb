6f25f6689d3a006dae0d400b72dcd8ed736e3f38
Alex Castillo <wikitelmex@gmail.com>
2021-12-25 12:08:25 -0700

    summary
    - starting ui design
    - add singleton to entities

--- main.rb
+++ main.rb
@@ -1,1 +1,31 @@
 require 'pry'
+require './book'
+require './person'
+require './rental'
+require './student'
+require './teacher'
+
+class BooksEntity
+  
+  def initialize
+    @entity = new
+    @books = []
+  end
+
+  class << self
+    attr_reader :entity, :books
+  end
+
+  def create(book)
+    @books.push(book)
+  end
+end
+
+s1 = BooksEntity.entity
+sm1 = BooksEntity
+binding.pry
+s1.create('book1')
+s2 = BooksEntity.entity
+
+puts s1
+puts s2
