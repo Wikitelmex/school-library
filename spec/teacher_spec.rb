94d2dabacbc2e17c15f10582edf6a3dd3e9c43c6
ahmedadel56 <ahmdadl563@gmail.com>
2022-01-06 19:57:29 +0200

    Please fix revert error

---
+++ spec/teacher_spec.rb
@@ -0,0 +1,23 @@
+require_relative '../model/teacher'
+
+describe Teacher do
+  before :each do
+    @teacher = Teacher.new(23, 'math', 'Ahmed')
+  end
+
+  it 'shows instance of teacher class' do
+    expect(@teacher).to be_instance_of Teacher
+  end
+
+  it 'matches teacher name' do
+    expect(@teacher.name).to match 'Ahmed'
+  end
+
+  it 'matches the specialization' do
+    expect(@teacher.specialization).to match 'math'
+  end
+
+  it 'matches the age' do
+    expect(@teacher.age).to match 23
+  end
+end
