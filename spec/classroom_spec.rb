d8fee4c875f15ef7858d3c8e03379565d6365922
ahmedadel56 <ahmdadl563@gmail.com>
2022-01-06 20:32:25 +0200

    Test classroom class

--- spec/classroom_spec.rb
+++ spec/classroom_spec.rb
@@ -1,1 +1,15 @@
-p 'hello'
+require_relative '../model/classroom'
+
+describe Classroom do
+  before :each do
+    @classroom = Classroom.new('class 1 ')
+  end
+
+  it 'match instance of student' do
+    expect(@classroom).to be_instance_of Classroom
+  end
+
+  it 'match class lable' do
+    expect(@classroom.label).to match 'class 1'
+  end
+end
